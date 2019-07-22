1c0a7e23165cf863c67be8a7f841659ba86dfaf5
Ankan <abasu@ub.attlocal.net>
2019-07-21 23:39:33 -0400

    Inital code to talk to the front end. * created templates directory to store all html files that can be served with render_template commmand. * created a posts dictionary variable to mock expected database call return. * added code to posts.html to pass variables to the front end. * added Jinja2 template structure to programmatically {% %} parse variable passed from the back end. * example includes a for loop and if else block in poss.html

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,55 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
+
+
+# create some dummy data until we configure a database
+
+posts = [
+    {
+        'author' : 'Ankan Basu',
+        'title'  : "Learning Flask",
+        'content' : "glad that I have used Sinatra before!",
+        'date_posted': 'July 21, 2019'
+    },
+
+    {
+        'author' : 'Mormi Basu',
+        'title'  : "Using Flask",
+        'content' : "Daddy is the best!",
+        'date_posted': 'July 21, 2019'
+    },
+
+    {
+        'author' : 'Mormi Basu',
+        'title'  : "Using Flask",
+        'content' : "Daddy is the best!",
+        'date_posted': 'July 21, 2019'
+    }
+
+
+]
+
+
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World Yes")
     #return 'Hello'
-    return f'<h1>Hello, {escape(name)}!</h1>'
+    #return f'<h1>Hello, {escape(name)}!</h1>'
+    return render_template('home.html') # render template us used to display a html file from templates folder
 
 @app.route('/about')
 def about():
     name = request.args.get("name", "About me")
-    #return 'Hello'
-    return f'<h1>Hello, {escape(name)}!</h1>'
+    #return f'<h1>Hello, {escape(name)}!</h1>'
+    return render_template('about.html')
+
+
+@app.route('/posts')
+def render_posts():
+    ## render the template and pass the posts dictionary to the frontend
+    return render_template('posts.html', posts=posts, title='Greatest Posts')
 
 
 # export FLASK_APP=flaskblog.py
